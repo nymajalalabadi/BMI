e213467f02d640af0529a02964ef5fb4a4ed4334
nym jalalabadi <nima.jalalabadii@gmail.com>
2025-07-01 11:27:45 -0600

    Completed App

---
+++ BMI/MVVM/ModelViews/BMIViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BMI.MVVM.Models;
+
+namespace BMI.MVVM.ModelViews
+{
+    public class BMIViewModel
+    {
+        public BMIViewModel()
+        {
+            BMI = new Models.BMI();
+            BMI.Height = 180;
+            BMI.Weight = 73;
+        }
+
+        public  Models.BMI  BMI { get; set; }
+    }
+}
